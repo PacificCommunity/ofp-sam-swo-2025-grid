820af7e05aad1b492b2d27bd3cb0c49dcd2e9857
Arni Magnusson <thisisarni@gmail.com>
2025-07-06 17:24:23 +1100

    Add R folder

---
+++ R/01_run_models.R
@@ -0,0 +1,13 @@
+library(condor)
+source("utilities.R")
+options(width=160)
+
+session <- ssh_connect("nouofpsubmit")
+
+(jobs <- condor_dir())
+
+# Initial test
+
+full_submit("N_05_76_54Amax",     "ss_3.30.23.2")
+full_submit("N_05_76_54Amax_opt", "ss_3.30.23.2")
+full_submit("simple_small",       "ss_3.30.23.2")
