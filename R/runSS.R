34ac00efcd3cc33633c01424a79c76838f31c6b7
kyuhank <kh2064@gmail.com>
2025-07-15 15:19:57 +1100

    have 2 cores for background processing

--- R/runSS.R
+++ R/runSS.R
@@ -5,4 +5,4 @@ scenario_folders <- list.dirs(GridDir, full.names = FALSE, recursive = FALSE)
 results_parallel <- mclapply(scenario_folders, 
                              run_scenario_script, 
                              base_path = GridDir, 
-                             mc.cores = detectCores()-1)
+                             mc.cores = detectCores()-2)
