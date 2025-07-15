631fdd396604fc1f9de59c1bcfa01e740da08544
kyuhank <kh2064@gmail.com>
2025-07-16 09:12:10 +1100

    treat ss3 options as env variable

--- R/runSS.R
+++ R/runSS.R
@@ -8,7 +8,7 @@ scenario_folders <- list.dirs(GridDir, full.names = FALSE, recursive = FALSE)
 results_parallel <- mclapply(seq_along(scenario_folders), function(i) {
   run_scenario_script(scenario_folders[i], 
                       base_path = GridDir, 
-                      ss3_options = "", 
+                      ss3_options = SS3_OPTIONS, 
                       verbose = VERBOSE, 
                       scenario_number = i)
 }, mc.cores = 2)
