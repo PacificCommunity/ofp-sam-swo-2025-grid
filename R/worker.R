bffffd89554fde81e0dcac2d669b74f9057750ec
Arni Magnusson <thisisarni@gmail.com>
2025-07-31 08:07:06 +1100

    Unique rds filenames

--- R/worker.R
+++ R/worker.R
@@ -12,9 +12,10 @@ r4ss <- function(i)
   folder_2 <- "../Q_10_128_PICTCPUE"
   model_runs <- dir(c(folder_1, folder_2), full=TRUE)
 
-  # Select models of interest, prepare rds filenames
+  # Select models of interest, prepare unique rds filenames
   model_runs <- grepv("Growth-External", model_runs, invert=TRUE)
-  rds_files <- file.path("../rds", paste0(basename(model_runs), ".rds"))
+  rds_files <- file.path("../rds", paste0(basename(dirname(model_runs)), "_",
+                                          basename(model_runs), ".rds"))
 
   # Read model results into r4ss object
   cat("Processing model", i, "... ")
