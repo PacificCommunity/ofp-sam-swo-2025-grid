434e62e8667192262f4cd936197ddd8add6e37bc
Arni Magnusson <thisisarni@gmail.com>
2025-08-05 09:44:38 +1100

    Estimation uncertainty time series

--- R/30_estimation_uncertainty.R
+++ R/30_estimation_uncertainty.R
@@ -20,6 +20,23 @@ tab <- round(t(sapply(data.frame(BBmsy, FFmsy), mean_and_quantiles)), 2)
 
 sims <- lapply(model_list, estimation_uncertainty_ts, nsim=1000)
 
+probs <- c(0.05, 0.10, 0.25, 0.50, 0.75, 0.90, 0.95)
+
 sb <- sapply(sims, `[`, "sb")
 sb <- do.call(rbind, sb)
-sb <- apply(sb, 2, quantile, probs=c(0.05, 0.10, 0.25, 0.50, 0.75, 0.90, 0.95))
+sb <- apply(sb, 2, quantile, probs=probs)
+
+rec <- sapply(sims, `[`, "rec")
+rec <- do.call(rbind, rec)
+rec <- apply(rec, 2, quantile, probs=probs)
+
+ffmsy <- sapply(sims, `[`, "ffmsy")
+ffmsy <- do.call(rbind, ffmsy)
+ffmsy <- apply(ffmsy, 2, quantile, probs=probs)
+
+bbmsy <- sapply(sims, `[`, "bbmsy")
+bbmsy <- do.call(rbind, bbmsy)
+bbmsy <- apply(bbmsy, 2, quantile, probs=probs)
+
+save(sb, rec, ffmsy, bbmsy,
+     file="../model_list/estimation_uncertainty_ts.RData")
