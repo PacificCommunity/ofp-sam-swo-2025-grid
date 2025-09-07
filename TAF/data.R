3ee5272ecac113f658899e3591a93e9e3e610e66
Arni Magnusson <thisisarni@gmail.com>
2025-09-07 14:35:21 +1100

    Prepare data

--- TAF/data.R
+++ TAF/data.R
@@ -1,9 +1,27 @@
 # Prepare data, write CSV data tables
 
-# Before:
-# After:
+# Before: model_list.rds (boot/data)
+# After:  conv.csv, ensemble.rds (data)
 
 library(TAF)
+source("boot/software/utilities_r4ss.R")
 
 mkdir("data")
 
+model_list <- readRDS("../model_list/model_list.rds")
+
+# Convergence
+conv <- data.frame(Model=names(model_list))
+conv$Gradient <- unlist(sapply(model_list, `[`, "maximum_gradient_component"))
+conv$log_det_hessian <- unlist(sapply(model_list, `[`, "log_det_hessian"))
+conv$PDH <- conv$log_det_hessian > 1e-6  # safer than > 0.0 floating point
+conv <- model_info(conv)
+
+# Create ensemble
+ensemble <- create_ensemble(model_list)
+ensemble <- list(tseries=model_info(ensemble$tseries),
+                 refpts=model_info(ensemble$refpts))
+
+# Write convergence table and ensemble list
+write.taf(conv, dir="data")
+saveRDS(ensemble, "data/ensemble.rds")
