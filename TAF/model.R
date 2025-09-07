e2bc34ff1cb6f90a278e07431c9e684fa3ce7e74
Arni Magnusson <thisisarni@gmail.com>
2025-09-07 14:35:37 +1100

    Run analysis

--- TAF/model.R
+++ TAF/model.R
@@ -1,9 +1,47 @@
 # Run analysis, write model results
 
-# Before:
-# After:
+# Before: model_list.rds (boot/data)
+# After:  uncertainty_refpt.rds, uncertainty_ts.rds (model)
 
 library(TAF)
+source("boot/software/utilities_r4ss.R")
 
 mkdir("model")
 
+# Read model results
+model_list <- readRDS("boot/data/model_list.rds")
+
+# Simulate reference point draws to incorporate estimation uncertainty
+set.seed(1)
+nsim <- 1000
+draws <- lapply(model_list, estimation_uncertainty, n=nsim)
+
+# Uncertainty about reference points
+BBmsy <- sapply(draws, `[`, "BBmsy")
+BBmsy <- unlist(BBmsy, use.names=FALSE)
+FFmsy <- sapply(draws, `[`, "FFmsy")
+FFmsy <- unlist(FFmsy, use.names=FALSE)
+uncertainty_refpt <- data.frame(BBmsy, FFmsy)
+
+# Uncertainty about time series
+message("Simulating time series")
+sims <- lapply(model_list, estimation_uncertainty_ts, nsim=nsim)
+message("Calculating quantiles")
+probs <- c(0.05, 0.10, 0.25, 0.50, 0.75, 0.90, 0.95)
+sb <- sapply(sims, `[`, "sb")
+sb <- do.call(rbind, sb)
+sb <- apply(sb, 2, quantile, probs=probs)
+rec <- sapply(sims, `[`, "rec")
+rec <- do.call(rbind, rec)
+rec <- apply(rec, 2, quantile, probs=probs)
+ffmsy <- sapply(sims, `[`, "ffmsy")
+ffmsy <- do.call(rbind, ffmsy)
+ffmsy <- apply(ffmsy, 2, quantile, probs=probs)
+bbmsy <- sapply(sims, `[`, "bbmsy")
+bbmsy <- do.call(rbind, bbmsy)
+bbmsy <- apply(bbmsy, 2, quantile, probs=probs)
+uncertainty_ts <- list(SB=sb, Rec=rec, FFmsy=ffmsy, BBmsy=bbmsy)
+
+# Save list objects
+saveRDS(uncertainty_refpt, "model/uncertainty_refpt.rds")
+saveRDS(uncertainty_ts, "model/uncertainty_ts.rds")
