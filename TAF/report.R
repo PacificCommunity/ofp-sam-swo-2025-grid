974f87529f7c071903e20aec4453996d8cf9b5a2
Arni Magnusson <thisisarni@gmail.com>
2025-09-08 20:36:47 +1100

    Round reference point table

--- TAF/report.R
+++ TAF/report.R
@@ -1,9 +1,23 @@
 # Produce plots and tables for report
 
-# Before:
-# After:
+# Before: tseries_est.rds (model), refpts.csv, tseries.csv (output)
+# After:  refpts_table.csv (report)
 
 library(TAF)
+source("boot/software/utilities_r4ss.R")
 
 mkdir("report")
 
+# Load reference points and time series
+refpts <- read.taf("output/refpts.csv")
+refpts_table <- read.taf("output/refpts_table.csv")
+tseries <- read.taf("output/tseries.csv")
+tseries_est <- readRDS("model/tseries_est.rds")
+
+# Round reference point table
+refpts_table <- taf2xtab(refpts_table)
+refpts_table <- round(refpts_table, 2)
+refpts_table[refpts_table > 100] <- round(refpts_table[refpts_table > 100])
+
+# Write table
+write.taf(refpts_table, dir="report")
